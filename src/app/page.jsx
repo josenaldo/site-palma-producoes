dd63487357792d2dddd6b7845636cf263639d379
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-29 15:17:18 -0300

    :sparkles: feat: Mudar titulo da home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,7 @@
-import { Typography } from '@mui/material'
-import Image from 'next/image'
-
 export default function HomePage() {
   return (
     <div>
-      <Typography variant="h1">Palma Produções</Typography>
+      <h1 variant="h1">Palma Produções</h1>
     </div>
   )
 }
