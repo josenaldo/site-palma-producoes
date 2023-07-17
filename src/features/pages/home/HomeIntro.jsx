e385fac0bcca33b2d4e034103036c5c0c4b4f6bd
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-07-17 20:25:43 -0300

    :recycle: refactor: Remoção do texto da home intro

--- src/features/pages/home/HomeIntro.jsx
+++ src/features/pages/home/HomeIntro.jsx
@@ -4,22 +4,14 @@ import { YoutubeVideo } from '@/features/ui'
 
 export default function HomeIntro({ t }) {
   return (
-    <Box
-      component="section"
-      sx={{
-        py: 5,
-      }}
-    >
+    <Box component="section">
       <Container>
-        <Stack spacing={2} justifyContent="center" alignItems="center">
-          <Typography variant="sectionTitle" component="h2">
-            {t('home:intro.title')}
-          </Typography>
+        <Stack justifyContent="center" alignItems="center">
           <Stack component="span" justifyContent="center" alignItems="center">
-            <Typography>{t('home:intro.subtitle')}</Typography>
             <KeyboardArrowDownOutlinedIcon
               sx={{
-                fontSize: '4rem',
+                fontSize: '6rem',
+                color: 'text.dark',
               }}
             />
           </Stack>
