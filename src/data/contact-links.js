14086122beaa4d501d389c9261a6709eb1746090
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-11-05 16:36:01 -0300

    :bug: fix: Correção no email, telefone e rodapé do site

--- src/data/contact-links.js
+++ src/data/contact-links.js
@@ -4,14 +4,14 @@ import EmailIcon from '@mui/icons-material/Email'
 export const contactLinks = [
   {
     title: 'Telefone',
-    text: '+55 (21) 9 9999-9999',
-    href: 'https://whatsa.me/5521123456789',
+    text: '+55 (21) 97977-9636',
+    href: 'https://whatsa.me/5521979779636',
     icon: WhatsAppIcon,
   },
   {
     title: 'E-mail',
-    text: 'contato@palmaproducoes.com',
-    href: 'mailto:contato@palmaproducoes.com',
+    text: 'contato@palmaproducoes.com.br',
+    href: 'mailto:contato@palmaproducoes.com.br',
     icon: EmailIcon,
   },
 ]
